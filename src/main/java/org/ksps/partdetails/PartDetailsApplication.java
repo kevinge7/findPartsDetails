0604963c65a369f0ddef0b4812863b7a1816f9f4
kevinge7 <yuxig5@bu.edu>
2025-08-28 17:11:38 -0400

    create springboot framework

---
+++ src/main/java/org/ksps/partdetails/PartDetailsApplication.java
@@ -0,0 +1,13 @@
+package org.ksps.partdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartDetailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PartDetailsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ksps/partdetails/PartDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ksps.partdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartDetailsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
